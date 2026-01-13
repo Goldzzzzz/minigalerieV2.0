dcb30d7a81eb73c915f5f33d2fad6c08ad2a6028
Goldzzzzz <soldatmendez777@gmail.com>
2026-01-12 23:46:14 -0500

    Fix signup import db.js

--- backend/dist/signup.js
+++ backend/dist/signup.js
@@ -1,6 +1,6 @@
-import { pool } from "./db";
+import { pool } from "./db.js";
 import bcrypt from "bcrypt";
-import { signToken } from "./auth";
+import { signToken } from "./auth.js";
 export async function signup(email, password) {
     const hashed = await bcrypt.hash(password, 10);
     const result = await pool.query("INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id", [email, hashed]);
